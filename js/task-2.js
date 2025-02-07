3e4c875c7cc428c8c4e6f0176050d5ba5f1543eb
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-07 20:47:00 +0600

    added task-2

---
+++ js/task-2.js
@@ -0,0 +1,14 @@
+/* 
+    ### 2. Add or remove elements
+    1. Declare an array of 3 tourist destinations
+    2. Add a new tourist destination to your tourist array
+    3. Add two more to your array
+    4. Remove the last tourist destination you have added
+    5. display the final array as output
+*/
+const touristPorts = ["Cox's Bazar", "Sundarbans", "Srimangal"];
+touristPorts.push("Rangamati");
+touristPorts.push("Saint Martin Island");
+touristPorts.push("Lalbagh Fort");
+touristPorts.pop();
+console.log(touristPorts);
