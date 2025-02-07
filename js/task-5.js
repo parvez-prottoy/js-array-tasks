676103b5f8a58d90bf875705e2b8a36618c2b7a8
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-07 21:14:05 +0600

    added task-5

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+/* 
+    ### 5. Combining Arrays
+
+    **Instructions:**
+
+    1. Create two arrays of your choice.
+    2. Use the concat method to combine the two arrays into a new array.
+    3. Print both the original arrays and the combined array using console.log().
+*/
+const frontend = ["HTML", "CSS", "JS", "React"];
+const backend = ["Node", "Express", "MongoDB"];
+const skills = frontend.concat(backend);
+console.log(frontend);
+console.log(backend);
+console.log(skills);
