fbbfb4a30c5dc046149cc1a97e4c9993d73d6e3c
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-07 20:41:41 +0600

    added task-1

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+/* 
+    ### 1. Declare an array 
+    1. Declare an array with 5 elements containing fruits
+    2. console log the 3rd index element
+    3. change the value of the 2nd index element to jambura
+    4. console log the final array
+*/
+const fruits = ["Banana", "Apple", "Strawberry", "Avocado", "Pineapple"];
+console.log(fruits[3]);
+fruits[2] = "Jambura";
+console.log(fruits);
